c1dd1a3b1eb9e17d4b14bbe56465228e2ba8f3d9
mbandi <bandimatias@gmail.com>
2021-06-13 12:38:57 +0200

    makeCacheMatrix and solveCache funciontality

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## makeCacheMatrix stores the original and inverted matrices, while cacheSolve 
+## returns the inverse of a matrix, and save it in the cache if it was not 
+## previously resolved
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## getter and setter functions for the original and inverted matrices
+makeCacheMatrix <- function(originalMatrix = matrix()) {
+  invertedMatrix <- NULL
+  
+  get <- function() originalMatrix
+  
+  set <- function(matrix) {
+    originalMatrix <<- matrix
+    invertedMatrix <<- NULL
+  }
+  
+  getInverted <- function() invertedMatrix
+  
+  setInverted <- function(inverted) invertedMatrix <<- inverted
+  
+  list(get = get, 
+       set = set,
+       getInverted = getInverted,
+       setInverted = setInverted)
 }
 
-
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x' and stores the result in a cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverted <- x$getInverted()
+  if (!is.null(inverted)) {
+    message("getting cached matrix")
+    return (inverted)
+  }
+  
+  message("calculating new matrix")
+  inverted <- solve(x$get(), ...)
+  x$setInverted(inverted)
+  inverted
 }
